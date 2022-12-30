daeaa651174fe17b046286222d073f108e3b8247
Daria Ivanova <dariaivsmai@gmail.com>
2022-12-30 22:32:36 +0300

    commit

--- HW3/Task 1/HW3/HW3.cpp
+++ HW3/Task 1/HW3/HW3.cpp
@@ -2,41 +2,54 @@
 
 class Calculator {
 private:
-    double num1, num2;
+    double numb1, numb2;
 public:
+    
+    double GetNum1() {
+        return numb1;
+    };
 
-    Calculator(double num1, double num2) {};
+    void SetNum1(double numb1) {
+        this->numb1 = numb1;
+    };
+    double GetNum2() {
+        return numb2;
+    };
 
-    double add(double &num1,double &num2) {
-        return num1 + num2;
+    void SetNum2(double numb2) {
+        this->numb2 = numb2;
     };
-    double multiply(double& num1, double& num2) {
-        return num1 * num2;
+
+    double add() {
+        return numb1 + numb2;
     };
-    double subtract_1_2(double& num1, double& num2) {
-        return num2 - num1;
+    double multiply() {
+        return numb1 * numb2;
     };
-    double subtract_2_1(double& num1, double& num2) {
-        return num1 - num2;
+    double subtract_1_2() {
+        return numb2 - numb1;
     };
-    double divide_1_2(double& num1, double& num2) {
-        return num1 / num2;
+    double subtract_2_1() {
+        return numb1 - numb2;
     };
-    double divide_2_1(double& num1, double& num2) {
-        return num2 / num1;
+    double divide_1_2() {
+        return numb1 / numb2;
     };
-    bool set_num1(double& num1) {
-        if (num1 != 0) {
-            this-> num1 = num1;
+    double divide_2_1() {
+        return numb2 / numb1;
+    };
+     bool set_num1(double numb1) {
+        if (numb1 != 0) {
+            this-> numb1 = numb1;
             return true;
         }
         else {
             return false;
         };
     };
-    bool set_num2(double& num2) {
-        if (num1 != 0) {
-            this->num2 = num2;
+    bool set_num2(double numb2) {
+        if (numb1 != 0) {
+            this->numb2 = numb2;
             return true;
         }
         else {
@@ -49,28 +62,36 @@ int main(int argc, char** argv)
 {
     setlocale(LC_ALL, "Russian");
     double num1, num2,oper;
+    Calculator number;
     while (true) {
         std::cout << "Введите первое число: " << std::endl;
-        std::cin >> num1;
+        std::cin >>num1;
+        number.SetNum1(num1);
+        if (num1 == 0) {
+            std::cout << "Некорректное число! Введите,пожалуйста, другое число: " << std::endl;
+            std::cin >> num1;
+            number.SetNum1(num1);
+        };
         std::cout << "Введите второе число: " << std::endl;
         std::cin >> num2;
+        number.SetNum2(num2);
         if (num2 == 0) {
             std::cout << "Некорректное число! Введите,пожалуйста, другое число: " << std::endl;
             std::cin >> num2;
+            number.SetNum2(num2);
         };
-        Calculator number(num1, num2);
-        oper = number.add(num1, num2);
+      
+        oper = number.add();
         std::cout << "num1 + num2 = " << oper << std::endl;
-        oper = number.multiply(num1, num2);
+        oper = number.multiply();
         std::cout << "num1 * num2 = " << oper << std::endl;
-        oper = number.subtract_1_2(num1, num2);
+        oper = number.subtract_1_2();
         std::cout << "num2 - num1 = " << oper << std::endl;
-        oper = number.subtract_2_1(num1, num2);
+        oper = number.subtract_2_1();
         std::cout << "num1 - num2 = " << oper << std::endl;
-        oper = number.divide_1_2(num1, num2);
+        oper = number.divide_1_2();
         std::cout << "num1 / num2 = " << oper << std::endl;
-        oper = number.divide_2_1(num1, num2);
-        std::cout << "num2 / num1 = " << oper << std::endl;
+
     };
     return 0;
 };
