cec71eacf4b7271d46a58b76f6eed0086d7cce10
Daria Ivanova <dariaivsmai@gmail.com>
2023-01-08 20:21:15 +0300

    HW3 task 1

--- HW3/Task 1/HW3/HW3.cpp
+++ HW3/Task 1/HW3/HW3.cpp
@@ -9,15 +9,25 @@ public:
         return numb1;
     };
 
-    void SetNum1(double numb1) {
-        this->numb1 = numb1;
+    bool SetNum1(double numb1) {
+        if (numb1 != 0) {
+            this->numb1 = numb1;
+        }
+        else {
+            return false;
+        };
     };
     double GetNum2() {
         return numb2;
     };
 
-    void SetNum2(double numb2) {
-        this->numb2 = numb2;
+    bool SetNum2(double numb2) {
+        if (numb2 != 0) {
+            this->numb2 = numb2;
+        }
+        else {
+            return false;
+        };
     };
 
     double add() {
@@ -38,24 +48,6 @@ public:
     double divide_2_1() {
         return numb2 / numb1;
     };
-     bool set_num1(double numb1) {
-        if (numb1 != 0) {
-            this-> numb1 = numb1;
-            return true;
-        }
-        else {
-            return false;
-        };
-    };
-    bool set_num2(double numb2) {
-        if (numb1 != 0) {
-            this->numb2 = numb2;
-            return true;
-        }
-        else {
-            return false;
-        };
-    };
 };
 
 int main(int argc, char** argv)
@@ -66,16 +58,16 @@ int main(int argc, char** argv)
     while (true) {
         std::cout << "Введите первое число: " << std::endl;
         std::cin >>num1;
-        number.SetNum1(num1);
-        if (num1 == 0) {
+
+        if (number.SetNum1(num1)==false) {
             std::cout << "Некорректное число! Введите,пожалуйста, другое число: " << std::endl;
             std::cin >> num1;
             number.SetNum1(num1);
         };
         std::cout << "Введите второе число: " << std::endl;
         std::cin >> num2;
-        number.SetNum2(num2);
-        if (num2 == 0) {
+        
+        if (number.SetNum2(num2) == false) {
             std::cout << "Некорректное число! Введите,пожалуйста, другое число: " << std::endl;
             std::cin >> num2;
             number.SetNum2(num2);
